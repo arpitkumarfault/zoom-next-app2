855204ccc0c112dd4ca3852a9deb39ac6a5660dc
kanwararpit <arpit.kanwar@quarkifi.com>
2025-01-15 08:56:00 +0530

    sixth commit

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -1,28 +1,35 @@
-'use server'
+"use server";
 
-import { currentUser } from '@clerk/nextjs/server'
-import { StreamClient } from '@stream-io/node-sdk'
+import { currentUser } from '@clerk/nextjs/server';
+import { StreamClient } from '@stream-io/node-sdk';
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY
-const apiSecret = process.env.STREAM_API_SECRET
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
 export const tokenProvider = async () => {
-  const user = await currentUser()
-  if (!user) throw new Error('No user not logged in 😕😕')
-  if (!apiKey) throw new Error('No Stream API Key provided')
-  if (!apiSecret) throw new Error('No Stream API Secret provided')
+  const user = await currentUser();
+  if (!user) throw new Error('No user logged in 😕😕');
+  if (!apiKey) throw new Error('No Stream API Key provided');
+  if (!apiSecret) throw new Error('No Stream API Secret provided');
 
-  const client = new StreamClient(apiKey, apiSecret)
+  const client = new StreamClient(apiKey, apiSecret);
+
   const newUser = {
     id: user.id,
     role: 'user',
     custom: {
       color: 'red',
     },
-    name: user.username,
+    name: user.username || "Guest", // Provide a default value when username is null
     image: user.imageUrl,
-  }
-  await client.upsertUsers([newUser])
-  const validity = 60 * 60
-  const token = await client.generateUserToken({ user_id: user.id, validity_in_seconds: validity })
-  return token
-}
+  };
+
+  await client.upsertUsers([newUser]);
+  const validity = 60 * 60;
+  const token = await client.generateUserToken({
+    user_id: user.id,
+    validity_in_seconds: validity,
+  });
+
+  return token;
+};
